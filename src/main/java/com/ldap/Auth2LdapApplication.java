5a9f38a13bdaf001856e536d2c06fe8b9043d69f
Savaş Dede <svsdd22@gmail.com>
2022-08-26 09:09:51 +0300

    init commit

---
+++ src/main/java/com/ldap/Auth2LdapApplication.java
@@ -0,0 +1,13 @@
+package com.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2LdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2LdapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldap/Auth2LdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2LdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
